e3a0c1cceeff5fc72614da0ba5cf15cb53e8c868
Igor Prokhorchuk <igor@prokhorchuk.com>
2024-11-08 21:28:36 +0100

    Initial commit

---
+++ src/main/java/de/friseur/friseur/FriseurApplication.java
@@ -0,0 +1,13 @@
+package de.friseur.friseur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriseurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriseurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/friseur/friseur/FriseurApplicationTests.java
@@ -0,0 +1,13 @@
+package de.friseur.friseur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriseurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
