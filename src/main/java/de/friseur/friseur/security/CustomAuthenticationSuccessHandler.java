b00db5b096d42ae8b08b69b73657731ba886f865
Igor Prokhorchuk <igor@prokhorchuk.com>
2025-10-26 17:51:16 +0100

    fix: update redirect path for admin role to /admin-dashboard

--- src/main/java/de/friseur/friseur/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/de/friseur/friseur/security/CustomAuthenticationSuccessHandler.java
@@ -17,7 +17,7 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 
         for (GrantedAuthority authority : authorities) {
             if (authority.getAuthority().equals("ROLE_ADMIN")) {
-                response.sendRedirect("/admin");
+                response.sendRedirect("/admin-dashboard");
                 return;
             }
         }
